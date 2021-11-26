6ea0546c0ea43b94d02221151d44072c5977fa27
Mirawara <lorenzo.mirabella@outlook.it>
2021-11-26 20:10:05 +0100

    Clients and WS
    Structure and beh of Clients and WebServer

---
+++ src/WebServer.h
@@ -0,0 +1,36 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __NCLIENTS_SERVER_SIM_WEBSERVER_H_
+#define __NCLIENTS_SERVER_SIM_WEBSERVER_H_
+
+#include <omnetpp.h>
+#include <queue>
+
+using namespace omnetpp;
+
+/**
+ * TODO - Generated class
+ */
+class WebServer : public cSimpleModule
+{
+  protected:
+    double serv_rate_w_;
+    queue queue_;
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+#endif
